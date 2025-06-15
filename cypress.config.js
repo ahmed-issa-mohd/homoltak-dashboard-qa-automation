fe8fe6c8c887d65bc3b6732057a37e699b87e7f9
ahmed-issa-mohd <ahmed.issa.mohd@gmail.com>
2025-06-15 18:14:45 +0300

    Resolved merge conflicts from governorates

---
+++ cypress.config.js
@@ -0,0 +1,15 @@
+const { defineConfig } = require("cypress");
+
+
+module.exports = defineConfig({
+    projectId: 'pifptg',
+    e2e: {
+        baseUrl: 'https://stg.homoltak.com', // الرابط الأساسي لتطبيق حمولتك
+        chromeWebSecurity: false, // يسمح بفتح روابط من نفس الصفحة بدون مشاكل CORS (مفيد لبعض الحالات)
+        experimentalStudio: true, // لتفعيل ميزة Cypress Studio (إنشاء خطوات الاختبار تلقائيًا من UI)
+        setupNodeEvents(on, config) {
+            // أحداث يمكن إضافتها هنا لاحقًا
+        },
+    },
+});
+
